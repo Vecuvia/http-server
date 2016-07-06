d6293ac1342e42a3f19427224f7db57390813282
mftrhu <mftrhu+github@inventati.org>
2016-07-07 00:32:38 +0200

    Add comments to `server.py`

--- server.py
+++ server.py
@@ -14,17 +14,27 @@ class Client(object):
         self.socket.setblocking(0)
         self.data = b""
     def receive(self, buffer_size):
+        """
+        Reads data from the socket in the client's buffer, and returns the
+        number of bytes read.
+        """
         data = self.socket.recv(buffer_size)
         if data:
             self.data += data
         return len(data)
     def send(self, data):
+        """
+        Sends a response or some raw data to the client's socket.
+        """
         if type(data) is Response:
             data = data.make()
         if type(data) is str:
             data = data.encode()
         self.socket.send(data)
     def close(self):
+        """
+        Closes the client's socket.
+        """
         try:
             self.socket.shutdown(socket.SHUT_WR)
         except OSError:
@@ -103,6 +113,9 @@ class BaseServer(object):
         self.server.bind((self.config["ADDRESS"], self.config["PORT"]))
         self.server.listen(self.config["BACKLOG"])
     def load_config(self, config):
+        """
+        Loads a json configuration file or a dictionary into `self.config`.
+        """
         if config is None:
             config = {}
         elif type(config) is str:
@@ -113,6 +126,9 @@ class BaseServer(object):
                 pass
         self.config.update(config)
     def serve_forever(self):
+        """
+        Starts the server.
+        """
         print("Serving on port {0}".format(self.config["PORT"]))
         try:
             while True:
@@ -120,36 +136,63 @@ class BaseServer(object):
         except KeyboardInterrupt:
             pass
     def accept(self):
+        """
+        Accepts a new client, adding it to `read_list`.
+        """
         client = Client(*self.server.accept())
+        #This is redundant but avoids us having to iterate through the clients
+        # list to extract their sockets.
         self.clients[client.socket] = client
         self.read_list.append(client.socket)
-        #print(client)
     def read_from(self, sock):
+        """
+        Reads data from a socket, handling (mostly) gracefully a closed 
+        connection.
+        """
         client = self.clients[sock]
         data_length = client.receive(self.config["BUFFER_SIZE"])
         if data_length == 0:
             self.drop_client(client)
             return
+        # We read BUFFER_SIZE bytes at a time - if we get less than that,
+        #  then this connection has `no_more_data` for us.
         no_more_data = data_length < self.config["BUFFER_SIZE"]
         if no_more_data:
+            # This is possibly redundant
             if socket not in self.write_list:
+                # The socket is finished and it's waiting for an answer.
                 self.write_list.append(sock)
-                #print(time.time(), client.data)
     def parse_request(self, client):
         return Request.parse(client.data)
     def make_error(self, error_code, error_message):
+        """
+        Utility function - returns an error response with a lightweight,
+        utterly uninformative html page describing it.
+        """
         return Response(self.config["VERSION"], error_code, error_message,
             headers={"Content-type": "text/html"},
             content=open(self.config["ERROR_PAGE"]).read().format(
                 error_code=error_code, error_message=error_message))
     def make_redirect(self, location):
+        """
+        Utility function - returns a 303 See Other redirect to `location`.
+        """
         return Response(self.config["VERSION"], 303, "See Other",
             headers={"Location": location})
     def serve_file(self, mime_type, content):
+        """
+        Utility function - returns a 200 Ok response with mime type `mime_type`
+        and content `content`.
+        """
         return Response(self.config["VERSION"], 200, "Ok",
             headers={"Content-type": mime_type},
             content=content)
     def make_response(self, request):
+        """
+        Despite the name, it doesn't make any responses but instead
+        dispatches the appropriate method, returning a 501 Not Implemented
+        error if no such method is found.
+        """
         if request.malformed:
             return self.make_error(400, "Bad Request")
         method = "do_" + request.method.upper()
@@ -164,12 +207,16 @@ class BaseServer(object):
             response = self.make_response(request)
             client.send(response)
             log_response(client, request, response)
-            #client.socket.send(b"HTTP/1.0 200 OK\r\n\r\nHello, world.")
         except (BrokenPipeError, ConnectionResetError) as e:
             pass
         finally:
+            #TODO: No keep-alive is implemented yet.
             self.drop_client(client)
     def drop_client(self, client):
+        """
+        Drops a client, removing all referencies to it and closing its
+        socket.
+        """
         sock = client.socket
         if sock in self.write_list:
             self.write_list.remove(sock)
@@ -183,14 +230,18 @@ class BaseServer(object):
             self.read_list, self.write_list, self.read_list)
         for sock in self.readable:
             if sock is self.server:
+                # We have a new client - let us `accept` it.
                 self.accept()
             else:
+                # Otherwise, we still need to get some data from the socket.
                 self.read_from(sock)
         for sock in self.writable:
             self.reply_to(sock)
     def sanitize(self, path):
+        #TODO: don't do this, it's possibly silly
         return "/".join(filter(lambda p: p not in (".", ".."), path.split("/")[1:]))
     def split_uri(self, uri):
+        #TODO: don't do this, it's silly
         uri, _, query_string = uri.partition("?")
         return uri, query_string
     def get_mimetype(self, uri):
@@ -203,10 +254,19 @@ class BaseServer(object):
                 ("application/octet-stream", True))
 
 class StaticFileServer(BaseServer):
+    """
+    Simple static file server built on top of `BaseServer`, serves files
+    from its BASE_DIRECTORY directory.
+    """
     def get_file(self, uri):
+        """
+        Returns what file we need to serve - returns `dir/index.html` if
+        `uri` points to a directory.
+        """
         uri, query_string = self.split_uri(uri)
         path = self.sanitize(uri)
         path = os.path.join(self.basedir, path)
+        #TODO: Could this throw a FileNotFoundError?
         if os.path.isdir(path):
             path = os.path.join(path, "index.html")
         return path, query_string
